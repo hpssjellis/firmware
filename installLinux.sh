ee2fdaa34929e342fa5490d24754a928a36b5e17
Jeremy Ellis <keyfreemusic@gmail.com>
2019-01-07 22:31:00 -0800

    Update installLinux.sh

--- installLinux.sh
+++ installLinux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 ## dependencies at
-##https://github.com/particle-iot/firmware/blob/develop/docs/dependencies.md#1-download-and-install-dependencies
+## https://github.com/particle-iot/firmware/blob/develop/docs/dependencies.md#1-download-and-install-dependencies
 
 sudo apt-get update
 
@@ -11,31 +10,16 @@ sudo apt-get install wget
 sudo apt-get install node
 sudo apt-get install npm
 
-# grap the main particle firmware repository
+# grap the main particle firmware repository and put it in the present folder
 git clone https://github.com/particle-iot/firmware.git
-
-
-
 
 
 echo "Cloned the repository. Enter to continue"
 echo "Next: install gcc-arm-embedded"
-read
+echo ""
+##read
 
-
-
-
-
-##sudo add-apt-repository ppa:team-gcc-arm-embedded/ppa
-
-
-
-
-##sudo apt-get install gcc-arm-embedded
-
-
-
-
+## goofy way to install gcc-arm older version that works with the firmware
 wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
 bzip2 -d gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
 tar xf gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar
@@ -43,16 +27,14 @@ tar xf gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar
 sudo mv gcc-arm-none-eabi-5_3-2016q1 /usr/gcc-arm
 
 
-## Should make this more permanent.
+## Should make this more permanent, will need to run this path every time you open a terminal
 export PATH=$PATH:/usr/gcc-arm/bin
 
 arm-none-eabi-gcc --version
 echo "enter to continue"
 echo "Next: install dfu-util"
-read
-
-
-
+echo ""
+##read
 
 
 sudo apt-get install dfu-util
@@ -63,27 +45,23 @@ sudo cp 50-particle.rules /etc/udev/rules.d/
 dfu-util -V
 echo "Enter to continue"
 echo "next install libarchive-zip-perl"
-read
-
-
-
-
-
+echo ""
+##read
 
 sudo apt-get install libarchive-zip-perl
 
-
 echo "Enter to continue"
 echo "Next: install the particle client"
-read
-
+echo ""
+##read
 
 
 npm install -g particle-cli
 particle --version
 echo "Enter to continue"
-echo "Next: Changing to firmware main folder and make clean"
-read
+echo "Next: Changing to firmware main folder and make all PLATFORM=argon"
+echo ""
+##read
 
 
 
